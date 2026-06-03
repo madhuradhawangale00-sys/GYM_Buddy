eb19e944df2bb31cbaaae54b86ee36460a8426cd
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-03 23:11:09 +0530

    changes done

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+//component
+import WorkoutDetails from '../components/WorkoutDetails';
+
 
 const Home = () => {
+
+  const [workouts, setWorkout] = useState(null)
+
+  useEffect(()=>{
+     const fetchworkouts = async ()=> {
+       const response = await fetch('/api/workouts/')
+       const json = await response.json()
+
+       if(response.ok){
+        setWorkout(json)
+       }
+     }
+     fetchworkouts()
+  },[])
+
+//key={workout._id}
   return (
     <div>
       <div className='home'>
-        <h2>
-            Home Page
-        </h2>
+        <div className='workouts'>
+          {
+            workouts && workouts.map((workout)=>(
+              // <p key={workout._id}>{workout.title}</p>
+              <WorkoutDetails key={workout._id} workout={workout}/>
+           ))
+          }
+        </div>
       </div>
     </div>
   )
