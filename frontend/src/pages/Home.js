4b91dd14f7fe609cb1e79796dfb9c505410b3a4b
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-03 13:26:30 +0530

    navbar added

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='home'>
+        <h2>
+            Home Page
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
