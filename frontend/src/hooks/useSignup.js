a3db91470ee9f3fd22f711d3bf1836c6df381b99
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-15 11:32:46 +0530

    User Signup hook added

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -5,6 +5,7 @@ import {useAuthContext} from './useAuthContext'
 export const useSignup = () => {
     const [error, setError] = useState(null);
     const [isLoading, setisLoading] = useState(null);
+    const {dispatch} = useAuthContext()
 
     const signup = async (email, password) => {
         setisLoading(true)
@@ -15,5 +16,24 @@ export const useSignup = () => {
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({email, password})
         })
+
+        const json = await Response.json();
+
+        if(!response.ok){
+            setisLoading(false)
+            setError(json.error)
+
+        }
+        if(response.ok){
+            //save the user to browsers local storage
+            localStorage.setItem('user', JSON.stringify(json))
+
+            // update global auth context
+            dispatch({type: 'LOGIN', payload:json})
+
+            setisLoading(false)
+        }
+
     }
+    return {signup, isLoading, error}
 }
