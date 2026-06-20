72f12c16cae53dcdeceb2f5b3ab2e7a8e6864020
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-21 00:27:03 +0530

    login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,19 @@
-const { useState } = require("react");
+
+import  { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
+
+
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [login, error, isLoading] = useLogin()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password);
+        // console.log(email, password);
+        await login(email, password)
         
     }
 
@@ -22,7 +28,8 @@ const Login = () => {
             <label>Password:</label>
             <input type="password" onChange={(e)=> setPassword(e.target.value)} value={password} />
 
-            <button>Login</button>
+            <button disabled={isLoading}>Login</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
