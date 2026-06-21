6b597544853a23f190828eff2fc82fab06fa6995
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-21 22:46:31 +0530

    logut login signup

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
+
 
 
 const Navbar = () => {
 
   const {logout} = useLogout()
+  const {user} = useAuthContext()
+  
 
   const handleClick = () =>{
+    logout()
 
   }
   return (
@@ -19,13 +24,23 @@ const Navbar = () => {
             </h1>
             </Link>
             <nav>
-              <div>
+              {
+                user && (
+                  <div>
+                <span>{user.email}</span>
                 <button onClick={handleClick}>Log out </button>
               </div>
-              <div>
+                )
+              }
+              
+              {
+                !user && (
+                  <div>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Signup</Link>
               </div>
+                )
+              }
             </nav>
         </div>
     </header>
