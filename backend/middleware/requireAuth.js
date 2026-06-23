0b51c594efc68ab045451136e33df2131d5603c3
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-23 23:05:44 +0530

    creating middleware

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,3 +1,7 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel')
+
+
 const requireAuth = (req, res, next) => {
     //verify authentication
 
@@ -5,5 +9,16 @@ const requireAuth = (req, res, next) => {
 
     if(!authentication){
         return res.status(401).json({error:"Authentication token required"})
+    }
+
+    //Bearer hhdhgfh.dggfdhgf.hsfgdyy
+    const token = authentication.split(' ')[1]
+
+    try{
+        const {_id} = jwt.verify(token, process.env.SECRET)
+    }catch(error){
+        console.log(error)
+        res.status(401).json({error:'Request is not authorised'})
+        
     }
 }
