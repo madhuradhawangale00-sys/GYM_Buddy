60ba3793a8279a3e02ca97bc54a333bd0bd751bd
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-23 22:49:57 +0530

    creating middleware

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,3 +1,9 @@
 const requireAuth = (req, res, next) => {
-    
+    //verify authentication
+
+    const {authentication} = req.headers
+
+    if(!authentication){
+        return res.status(401).json({error:"Authentication token required"})
+    }
 }
